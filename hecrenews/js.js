e07d3d71346bf42b3f0b5665468f23671c0d7273
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2018-06-28 18:09:15 -0500

    Update js.js

--- hecrenews/js.js
+++ hecrenews/js.js
@@ -16,13 +16,3 @@ function addNewNews() {
 		element.appendChild(anchor);
 	}
 }
-
-//Responsive Navigation
-function openNav() {
-    	var x = document.getElementById("nav");
-    	if (x.className === "nav") {
-        	x.className += " responsive";
-    	} else {
-        	x.className = "nav";
-    	}
-}
