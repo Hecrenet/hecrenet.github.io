a051a6df04820f0f62b282a1977d924926350862
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2018-06-27 22:59:49 -0500

    Update js.js

--- hecrenews/js.js
+++ hecrenews/js.js
@@ -1,3 +1,20 @@
+var newNewsLinks = ["/hecrenews/index.html", "/index.html"];
+var newNewsTitles = ["Test", "2nd Anchor Test"];
+addNewNews();
+
+function addNewNews() {
+	var anchor;
+	var node;
+	var element = document.getElementById(colOne);
+	for(var i = 0; i < newNewsLinks.length; i++) {
+		anchor = document.createElement("a");
+		anchor.href = newNewsLinks[i];
+		node = newNewsTitles[i];
+		element.appendChlid(anchor);
+	}
+}
+
+//Responsive Navigation
 function openNav() {
     	var x = document.getElementById("nav");
     	if (x.className === "nav") {
