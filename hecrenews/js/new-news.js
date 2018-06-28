090e0b4a6fb521cb67a96dfe2bf45ac42c0055ed
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2018-06-28 18:51:10 -0500

    Update new-news.js

--- hecrenews/js/new-news.js
+++ hecrenews/js/new-news.js
@@ -2,6 +2,7 @@ var newNewsLinks = ["/hecrenews/index.html", "/index.html"];
 var newNewsTitles = ["Test", "2nd Anchor Test"];
 var newNewsImage = "/images/birds/legendary/hbird/hbird.jpg";
 
+//For every article
 function addColNewNews() {
 	var anchor;
 	var node;
@@ -11,20 +12,29 @@ function addColNewNews() {
 		anchor.href = newNewsLinks[i];
 		node = document.createTextNode(newNewsTitles[i]);
 		anchor.appendChild(node);
-		
 		element.appendChild(anchor);
 	}
 }
 
+//For the Hecrenews home page
 function addCardNewNews() {
 	var anchor;
 	var node;
+	var elements = document.getElementsByClassName("new-news");
 	//Add the image
 	document.getElementById("new-news").src = newNewsImage;
-	//Add the latest news!
-	var elements = document.getElementsByClassName("new-news");
-	for(var i = 0; i < elements.length; i++) {
+	//Add the latest news
+	anchor = document.createElement("a");
+	anchor.href=newNewsLinks[0];
+	node = document.createTextNode(newNewsTitles[0]);
+	anchor.appendChild(node);
+	document.getElementById("latest-news").appendChild(anchor);
+	//Add the rest
+	for(var i = 1; i < newNewsLinks.length; i++) {
 		anchor = document.createElement("a");
 		anchor.href = newNewsLinks[i];
+		node = document.createTextNode(newNewsTitles[i]);
+		anchor.appendChild(node);
+		element[i].appendChild(anchor);
 	}
 }
