3a2165abc6045bf91f87517ef0f0cb0de7fb4c2d
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2018-06-29 09:04:12 -0500

    Redoing the JavaScript
    Changing the logic from an array to a multi-dimension array so the code for adding the news articles won't have to be copied over and over again. It will save space and make it easier to edit.

---
+++ hecrenews/js/latest-news.js
@@ -0,0 +1,52 @@
+//Init Arrays
+var newsLinks = [
+	["/hecrenews/template.html", "#", "#", "#", "#"] //New News [0][x]
+	["#", "#", "#", "#", "#"] //Mundane [1][x]
+	["#", "#", "#", "#", "#"] //Gossip [2][x]
+	["#", "#", "#", "#", "#"] //Political [3][x]
+	["#", "#", "#", "#", "#"] //Economics [4][x]
+	["#", "#", "#", "#", "#"] //Technology [5][x]
+	["#", "#", "#", "#", "#"] //Fashion [6][x]
+	["#", "#", "#", "#", "#"] //Food [7][x]
+	["#", "#", "#", "#", "#"] //Travel [8][x]
+];
+var newsTitles = [
+	["Template", "No News :(", "No News :(", "No News :(", "No News :("] //New News [0][x]
+	["No News :(", "No News :(", "No News :(", "No News :(", "No News :("] //Mundane [1][x]
+	["No News :(", "No News :(", "No News :(", "No News :(", "No News :("] //Gossip [2][x]
+	["No News :(", "No News :(", "No News :(", "No News :(", "No News :("] //Political [3][x]
+	["No News :(", "No News :(", "No News :(", "No News :(", "No News :("] //Economics [4][x]
+	["No News :(", "No News :(", "No News :(", "No News :(", "No News :("] //Technology [5][x]
+	["No News :(", "No News :(", "No News :(", "No News :(", "No News :("] //Fashion [6][x]
+	["No News :(", "No News :(", "No News :(", "No News :(", "No News :("] //Food [7][x]
+	["No News :(", "No News :(", "No News :(", "No News :(", "No News :("] //Travel [8][x]
+];
+var newNewsImage = "/images/birds/legendary/hbird/hbird.jpg";
+
+
+//DON'T CHANGE BOTTOM CODE
+function addColNewNews(category, divName) {
+	var anchor, node, element = document.getElementById(divName);
+	for(var i = 0; i < newNewsLinks.length; i++) {
+		anchor = document.createElement("a");
+		anchor.href = newNewsLinks[category][i];
+		node = document.createTextNode(newNewsTitles[category][i]]);
+		anchor.appendChild(node);
+		element.appendChild(anchor);
+	}
+}
+function addCardNewNews(category) {
+	var anchor, node, elements = document.getElementsByClassName(category + "-news");
+	document.getElementById("new-news").src = newNewsImage;
+	for(var i = 0; i < newNewsLinks.length; i++) {
+		anchor = document.createElement("a");
+		anchor.href = newNewsLinks[category][i];
+		node = document.createTextNode(newNewsTitles[category][i]);
+		anchor.appendChild(node);
+		if (i > 0) {
+			elements[i].appendChild(anchor);
+		} else {
+			document.getElementById("latest-news").appendChild(anchor);
+		}
+	}
+}
