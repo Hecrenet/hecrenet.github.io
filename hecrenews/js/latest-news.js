f71a67132a926f2a5fe542bc1e259c5c700b1b74
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2018-07-04 12:07:41 -0500

    Update latest-news.js

--- hecrenews/js/latest-news.js
+++ hecrenews/js/latest-news.js
@@ -1,14 +1,14 @@
 //This is the only thing you change, or you might break the Hecrenews (unless your Arjvik, but the code shouldn't need changing)
 var newsLinks = [
-	["/hecrenews/template.html", "#", "#", "#", "#"], //New News [0][x]
-	["#", "#", "#", "#", "#"], //Mundane [1][x]
-	["#", "#", "#", "#", "#"], //Gossip [2][x]
-	["#", "#", "#", "#", "#"], //Political [3][x]
-	["#", "#", "#", "#", "#"], //Economics [4][x]
-	["#", "#", "#", "#", "#"], //Technology [5][x]
-	["#", "#", "#", "#", "#"], //Fashion [6][x]
-	["#", "#", "#", "#", "#"], //Food [7][x]
-	["#", "#", "#", "#", "#"] //Travel [8][x]
+	["/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html"], //New News [0][x]
+	["/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html"], //Mundane [1][x]
+	["/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html"], //Gossip [2][x]
+	["/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html"], //Political [3][x]
+	["/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html"], //Economics [4][x]
+	["/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html"], //Technology [5][x]
+	["/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html"], //Fashion [6][x]
+	["/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html"], //Food [7][x]
+	["/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html", "/hecrenews/nothing.html"] //Travel [8][x]
 ];
 //UNDER HERE IS THE SHTUFF YOU DON'T TOUCH
 addJQuery();
@@ -16,16 +16,12 @@ addJQuery();
 var newsTitles = [["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""]];
 var newNewsImages = ["", "", "", "", "", "", "", "", "",];
 
+addTitlesAndImages();
 
+//FUNCTIONS THAT GET CALLED FROM HTML
 function addColNews(category, divName) {
-	var anchor, node, element = $("#divName");
 	for(var i = 0; i < element.length; i++) {
-		//$($(element)[0]).append("<a href=" + newsLinks[category][i] + ">" + newsTitles[category][i] + "</a>");
-		anchor = document.createElement("a");
-		anchor.href = newsLinks[category][i];
-		node = document.createTextNode(newsTitles[category][i]);
-		anchor.appendChild(node);
-		element[0].appendChild(anchor);
+		$($("divName")[0]).append("<a href=" + newsLinks[category][i] + ">" + newsTitles[category][i] + "</a>");
 	}
 }
 
@@ -45,9 +41,24 @@ function addCardNews(category) {
 	}
 }
 
+//FUNCTIONS THAT GET USED
 function addJQuery() {
 	var script = document.createElement("script");
 	script.src = "jquery.js";
 	script.type = "text/javacsript";
 	document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+function addTitlesAndImages() {
+	for (var row = 0; row < newLinks.length; row++) {
+		//Image Links
+		$.ajax({url: newsLinks[row][0], type: "get", async: false, success: function(data){
+			newNewsImages[row] = $($(data).find("#thumbnail"))[0].src;
+		}});
+		for (var column = 0; column < newLinks[row].length; column++;) {
+			//Titles
+			$.ajax({url: newsLinks[row][column], type: "get", async: false, success: function(data){
+				newsTitles[row][column] = $($(data).find("#title"))[0].textContent;
+			}});
+	}
 }
