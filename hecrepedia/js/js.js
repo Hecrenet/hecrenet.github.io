9f4dc9e9f0f065a8119a41b74bdcb4b0564429fc
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2018-07-10 19:31:53 -0500

    Update js.js

--- hecrepedia/js/js.js
+++ hecrepedia/js/js.js
@@ -1,3 +1,14 @@
+
+//Add Tabs
+function addTabs(...names) {
+    for (var i = 0; i < names.length; i++) {
+        $($(".tab")[0]).append("<button class='tablinks' onlick=\"openTab(event," + names[i] + ")\">" + names[i] + "</button>");
+    }
+    $(".tab .tablinks")[0].class += 'active';
+    $(".tab .tablinks")[0].id = 'defaultOpen';
+}
+
+//Switch the content tabs
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
