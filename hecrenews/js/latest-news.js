c05d074916e2df3d4cb9e072af69d18d26563015
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2018-07-16 23:05:56 -0500

    Update latest-news.js

--- hecrenews/js/latest-news.js
+++ hecrenews/js/latest-news.js
@@ -18,11 +18,19 @@ var newsLinks = [
 function addCardNews(divName, link) {
 	$(divName).append("<div class='news-card'><div class= 'news-card-info'><ul><li><img></li><li><p></p></li><li><p></p></li></ul><div class='news-author-placeholder'></div></div><a class='need-link'><img src='/images/birds/legendary/hbird/hbird.jpg'></a><div class='news-card-flavor'><h2><a href='need-link'></a></h2><a href='javascript: void(0)' onclick='showNewsInfo(this)'><h2>&#x2193</h2></a><div class='news-card-preview'></div></div></div>");
 	//Add the information
-	if (typeof link == "string") {
-		
-	} else {
-		
-	} 			  	
+	$.get(link, function(data){
+		var authorImg, pubDate, pubTime, authorBio, articleTitle, articlePreview;
+		//Get the information from the author page (async false so no variable errors)
+		var authorLink = $(data).filter("#author-link")[0].innerHTML;
+		$.ajax({url: authorLink, type: "get", async: false, success: function(data){
+			authorImg = $(data).filter("img")[0].src;
+			authorBio = $(data).filter(".news-author")[0];
+		}});
+		pubDate = $(data).filter("#date")[0].innerHTML;
+		pubTime = $(data).filter("#time")[0].innerHTML
+		articleTitle = $(data).filter("#title")[0].innerHTML;
+		articlePreview = $(data).filter("#preview")[0].innerHTML;
+	});			  	
 }
 
 //FUNCTIONS THAT GET USED
