7b30b95340ee53eccdbf0ee78850b430de173ebe
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2018-07-22 11:31:18 -0500

    Update js.js

--- hecrepedia2/js/js.js
+++ hecrepedia2/js/js.js
@@ -1,1 +1,9 @@
-
+/*===================================
+  Functions that need DOM to load first
+  ===================================*/
+$(function() {
+	//Load in the Navigation Bar
+	$.ajax({url: "/hecrepedia2/nav.html", type: "get", async: false, success: function(data) {
+		$("#top-nav").append(data);
+	}});
+});
