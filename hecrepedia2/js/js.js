c934e0de48b6e7d77febc0d3d91de1a71db305de
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2018-07-22 11:46:22 -0500

    Update js.js

--- hecrepedia2/js/js.js
+++ hecrepedia2/js/js.js
@@ -7,3 +7,43 @@ $(function() {
 		$("#top-nav").append(data);
 	}});
 });
+/*===================================
+  Copied functions from first
+  Hecrepedia
+  ===================================*/
+//Add Card(s)
+function addCards(divName, ...links) {
+	var image, name, info;
+	for (var i = 0; i < links.length; i++) {
+		$.ajax({url: links[i], type: "get", async: false, success: function(data){
+			image = $(data).find("#infoImg")[0].src;
+			name = $(data).filter("#infoName")[0].textContent;
+			info = $(data).find("#info")[0].textContent;
+		}});
+		$(divName).append("<div class='card'><a href='" + links[i] + "'><img src='" + image + "'></a><div class='cardContainer'><p><b>" + name + "</b></p></div></div>");
+	}
+}
+
+//Add Tabs
+function addTabs(...names) {
+	for (var i = 0; i < names.length; i++) {
+		$($(".tab")[0]).append("<button class=\"tablinks\" onclick=\"openTab(event,'" + names[i] + "')\">" + names[i] + "</button>");
+	}
+	$($(".tab .tablinks")[0]).addClass("active");
+	$(".tab .tablinks")[0].id = "defaultOpen";
+}
+
+//Switch the content tabs
+function openTab(evt, tabName) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+}
