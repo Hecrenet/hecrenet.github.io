dc01f5d318464b20890ac04ea61b1fbaa61dbc65
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2018-07-23 10:42:12 -0500

    Update js.js

--- hecrepedia/js/js.js
+++ hecrepedia/js/js.js
@@ -1,5 +1,6 @@
 /*===================================
-  Functions that need DOM to load first
+  Functions that need all DOM objects
+  to load first
   ===================================*/
 $(function() {
 	//Load in the Navigation Bar
@@ -11,10 +12,10 @@ $(function() {
 });
 /*===================================
   Copied functions from first
-  Hecrepedia
+  Hecrepedia, compressed a few of them
   ===================================*/
 //Add Card(s)
-function addCards(divName, ...links) {
+function addAnimalCards(divName, ...links) {
 	var image, name, info;
 	for (var i = 0; i < links.length; i++) {
 		$.ajax({url: links[i], type: "get", async: false, success: function(data){
@@ -22,30 +23,11 @@ function addCards(divName, ...links) {
 			name = $(data).filter("#infoName")[0].textContent;
 			info = $(data).find("#info")[0].textContent;
 		}});
-		$(divName).append("<div class='card'><a href='" + links[i] + "'><img src='" + image + "'></a><div class='cardContainer'><p><b>" + name + "</b></p></div></div>");
+		$(divName).append("<div class='animal-card'><a href='" + links[i] + "'><img src='" + image + "'></a><div class='cardContainer'><p><b>" + name + "</b></p></div></div>");
 	}
 }
 
 //Add Tabs
-function addTabs(...names) {
-	for (var i = 0; i < names.length; i++) {
-		$($(".tab")[0]).append("<button class=\"tablinks\" onclick=\"openTab(event,'" + names[i] + "')\">" + names[i] + "</button>");
-	}
-	$($(".tab .tablinks")[0]).addClass("active");
-	$(".tab .tablinks")[0].id = "defaultOpen";
-}
-
+function addTabs(...names) {for (var i = 0; i < names.length; i++) {$($(".tab")[0]).append("<button class=\"tablinks\" onclick=\"openTab(event,'" + names[i] + "')\">" + names[i] + "</button>");}$($(".tab .tablinks")[0]).addClass("active");$(".tab .tablinks")[0].id = "defaultOpen";}
 //Switch the content tabs
-function openTab(evt, tabName) {
-	var i, tabcontent, tablinks;
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
-	}
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" active", "");
-	}
-	document.getElementById(tabName).style.display = "block";
-	evt.currentTarget.className += " active";
-}
+function openTab(evt, tabName) {var i, tabcontent, tablinks;tabcontent = document.getElementsByClassName("tabcontent");for (i = 0; i < tabcontent.length; i++) {tabcontent[i].style.display = "none";}tablinks = document.getElementsByClassName("tablinks");for (i = 0; i < tablinks.length; i++) {tablinks[i].className = tablinks[i].className.replace(" active", "");}document.getElementById(tabName).style.display = "block";evt.currentTarget.className += " active";}
