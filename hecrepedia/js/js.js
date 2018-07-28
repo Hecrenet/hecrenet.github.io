9b946cac748efcd8fe51de92c119126f5e032e6b
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2018-07-28 11:56:23 -0500

    Update js.js

--- hecrepedia/js/js.js
+++ hecrepedia/js/js.js
@@ -36,13 +36,11 @@ function addAnimalCards(divName, ...links) {
 	var image, name, information;
 	for (var i = 0; i < links.length; i++) {
 		$.ajax({url: links[i], type: "get", async: false, success: function(data){
-			name = $(data).find("#animal-name");
+			//name =
 			information = data.slice(data.search("fillOutPage"), data.length);
 			information = information.slice(information.search("\\(") + 1, information.search("\\)"));
 			information = information.split("\n");
-			image = information[2].slice(information[2].search('"') - 1, information[2].length - 1);
-			console.log(image);
-			console.log(name);
+			image = information[2].slice(information[2].search('"'), information[2].length - 1);
 		}});
 		//$(divName).append("");
 	}
