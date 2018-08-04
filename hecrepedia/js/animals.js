cf5d31dafd63e4af6d0c8813dfabdde445d2f385
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2018-08-04 12:46:41 -0500

    Update animals.js

--- hecrepedia/js/animals.js
+++ hecrepedia/js/animals.js
@@ -47,16 +47,6 @@ function addFactTable(...tableData) {
   to load first
   ===================================*/
 function waitForDOM() {
-	//Someone plz help, the changeCardGroupHeight() only works on page refresh, when button is clicked, or when page size changes
-	if(document.URL.indexOf("#")==-1) {
-		// Set the URL to whatever it was plus "#".
-		url = document.URL+"#";
-		location = "#";
-		//Reload the page
-		location.reload(true);
-	} else {
-		history.pushState(null, null, document.location.href.match(/(^[^#]*)/)[0]);
-	}
 	//Load in the Navigation Bar
 	$.ajax({url: "/hecrepedia/nav.html", type: "get", async: false, success: function(data) {
 		$("#top-nav").append(data);
