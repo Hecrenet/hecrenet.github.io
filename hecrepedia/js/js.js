f8fd792c5dc102e06bcb78ea6f0b28474b73584f
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2018-08-14 19:38:06 -0500

    Update js.js

--- hecrepedia/js/js.js
+++ hecrepedia/js/js.js
@@ -71,7 +71,7 @@ function createExtraInfo(parentElement, i, ...tabNames) {
 	for (var j = 0; j < tabNames.length; j++) {
 		$($(parentElement)[i]).append("<li class=" + tabNames[j] + " onclick='openExtraInfoContent(this)'><a href='javascript:void(0)'></a><hr><div class='" + tabNames[j] + "-content content'></div></li>");
 	}
-	$($(parentElement + " .bio a")[i]).HTML("<i class='fas fa-user'></li>");
+	$($(parentElement + " .bio a")[i]).html("<i class='fas fa-user'></li>");
 }
 
 function openExtraInfoContent(object) {
