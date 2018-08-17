e189ef15a1743491943e3ccaa47f1ca6f03e1529
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2018-08-17 18:15:15 -0500

    Update js.js

--- hecrenews/js/js.js
+++ hecrenews/js/js.js
@@ -1,6 +1,7 @@
 /*===================================
   Global Variables
   ===================================*/
+var WALLPAPER_SIZE = 500;
 
 /*===================================
   Wait for DOM Elements to load
@@ -8,6 +9,18 @@
 $(function(){
 	//Load in the navigation bar
 	$("#top-nav").load("/hecrenews/nav.html");
-  	//Set the background image of wallpaper divs
-	$(".wallpaper").css("background-image", "url(" + $(".wallpaper").attr('id') + ")");
+	wallpaperSetup();
 });
+
+/*===================================
+  Organized Functions
+  ===================================*/
+function wallpaperSetup() {
+	//Set the height and background image of wallpaper divs
+	$(".wallpaper").css({
+		"background-image" : "url(" + $(".wallpaper").attr('id') + ")",
+		"height" : String(WALLPAPER_SIZE) + "px";
+	});
+	//Center the box
+	$(".title-box").css("margin-top", String(WALLPAPER_SIZE - $(".title-box").height));
+}
