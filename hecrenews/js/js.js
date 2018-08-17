870f57729ef76d7b4cc6a80068c121f25cc4a175
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2018-08-17 17:48:11 -0500

    Update js.js

--- hecrenews/js/js.js
+++ hecrenews/js/js.js
@@ -6,6 +6,8 @@
   Wait for DOM Elements to load
   ===================================*/
 $(function(){
+	//Load in the navigation bar
+	$("#top-nav").load("/hecrenews/nav.html");
   	//Set the background image of wallpaper divs
 	$(".wallpaper").css("background-image", "url(" + $(".wallpaper").attr('id') + ")");
 });
