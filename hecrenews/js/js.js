da32b684eff07c1212394b51890f45e23f596a81
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2018-08-17 17:20:35 -0500

    Update js.js

--- hecrenews/js/js.js
+++ hecrenews/js/js.js
@@ -1,1 +1,10 @@
+/*===================================
+  Global Variables
+  ===================================*/
 
+/*===================================
+  Wait for DOM Elements to load
+  ===================================*/
+$(function(){
+	$(".wallpaper").css("background-image", "background-image: url(" + $(this).id + ")";
+});
