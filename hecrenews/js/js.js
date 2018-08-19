1e95837617f7389569cfa60326d16c22f89a3577
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2018-08-19 10:22:41 -0500

    Update js.js

--- hecrenews/js/js.js
+++ hecrenews/js/js.js
@@ -51,7 +51,7 @@ function createAnimalCard(i, divName, links) {
 		information = information.split("<li>");
 		information.splice(0, 1);
 		for (var j = 0; j < information.length; j++) {
-			information[j].slice(0, information[j].length - 4);
+			information[j].slice(0, 4);
 		}
 		console.log(information);
 		//Set the variables of the information
